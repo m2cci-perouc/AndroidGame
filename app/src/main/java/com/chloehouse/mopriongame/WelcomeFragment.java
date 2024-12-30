d36df22d571013803f170708bf26d1195b9cf5e6
m2cci-perouc <perouc@gmail.com>
2024-12-30 14:18:39 +0100

    enleve le clavier quand clique ailleurs sur le fragment

--- app/src/main/java/com/chloehouse/mopriongame/WelcomeFragment.java
+++ app/src/main/java/com/chloehouse/mopriongame/WelcomeFragment.java
@@ -1,11 +1,14 @@
 package com.chloehouse.mopriongame;
 
+import android.annotation.SuppressLint;
+import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
@@ -15,8 +18,10 @@ import android.text.Editable;
 import android.text.TextWatcher;
 import android.util.Pair;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -48,6 +53,7 @@ public class WelcomeFragment extends Fragment {
         button.setEnabled(!valeurJoueur1.isEmpty() && !valeurJoueur2.isEmpty());
     }
 
+    @SuppressLint("ClickableViewAccessibility")
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
@@ -57,6 +63,7 @@ public class WelcomeFragment extends Fragment {
         EditText textJoueur1 = view.findViewById(R.id.nameJoueur1);
         EditText textJoueur2 = view.findViewById(R.id.nameJoueur2);
         TextView tvPolitique = view.findViewById(R.id.tvPolitiqueConfidentialite);
+        ConstraintLayout constraintLayout = view.findViewById(R.id.fragment_welcome);
 
         // Appeler la méthode pour desactiver le bouton
         verifierEtActiverBouton(textJoueur1, textJoueur2, button);
@@ -68,6 +75,21 @@ public class WelcomeFragment extends Fragment {
                 Intent intent = new Intent(Intent.ACTION_VIEW);
                 intent.setData(Uri.parse("https://www.privacypolicies.com/live/671e4fb6-889b-4ba4-ad6f-0387e9840aaa"));
                 startActivity(intent);
+            }
+        });
+
+        // enleve le clavier si clique en dehors
+        constraintLayout.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                if (event.getAction() == MotionEvent.ACTION_DOWN) {
+                    View view = getActivity().getCurrentFocus();
+                    if (view instanceof EditText) {
+                        InputMethodManager imm = (InputMethodManager) getActivity().getSystemService(Context.INPUT_METHOD_SERVICE);
+                        imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
+                    }
+                }
+                return false;
             }
         });
 
